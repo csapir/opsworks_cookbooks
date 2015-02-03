60bc678a39c0c12d38744355e376ddd392e32bb4
Hen haim Sapir <chen.sapir@me.com>
2015-02-03 11:37:39 -0500

    Replaced hard coded values with references to attribute file

--- createdir/recipes/default.rb
+++ createdir/recipes/default.rb
@@ -1,10 +1,9 @@
-[ "srv/www/config", "/srv/www/shared" ].each do |path|
+[ "/srv/www/#{node['createdir']['shared_dir']}", "/srv/www/#{node['createdir']['config_dir']}" ].each do |path|
   directory path do
-    mode 0755
-    owner 'root'
-    group 'root'
+    mode node['createdir']['mode']
+    owner node['createdir']['owner']
+    group node['createdir']['group']
     recursive true
     action :create
   end
 end
-
