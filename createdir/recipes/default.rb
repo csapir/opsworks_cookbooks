559fe0cdb2bfb0ab4bcaf6d16a474fe9ab43a748
Hen haim Sapir <chen.sapir@me.com>
2015-02-03 11:22:55 -0500

    Adding multiple directories recursively

--- createdir/recipes/default.rb
+++ createdir/recipes/default.rb
@@ -1,6 +1,10 @@
-directory "/srv/www/" do
-  mode 0755
-  owner 'root'
-  group 'root'
-  action :create
+[ rv/www/config", "/srv/www/shared" ].each do |path|
+  directory path do
+    mode 0755
+    owner 'root'
+    group 'root'
+    recursive true
+    action :create
+  end
 end
+
